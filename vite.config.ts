9605c99f41e727ef977c1a7679e0f5087c59bc29
jéssica senger <jessicasbogoni1@gmail.com>
2023-01-09 19:21:33 -0400

    add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    vitePluginFaviconsInject('./src/assets/favicon.png'),
+  ],
 })
