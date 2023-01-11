3618ace3568e1f70b118c70507cd84e8a0536e00
jéssica senger <jessicasbogoni1@gmail.com>
2023-01-10 23:13:38 -0400

    creat test util to get element by text content
    finds element with text broken into multiple elements

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, render, screen } from '@testing-library/react'
 import { afterEach } from 'vitest'
 
 afterEach(() => {
@@ -11,6 +11,16 @@ const customRender = (ui: React.ReactElement, options = {}) =>
     ...options,
   })
 
+const getByTextContent = (text: string) => {
+  return screen.getByText((content, element) => {
+    const hasText = (element: Element | null) => element?.textContent === text
+    const elementHasText = hasText(element)
+    const childrenDontHaveText = Array.from(element?.children || []).every(child => !hasText(child))
+    return elementHasText && childrenDontHaveText
+  })
+}
+
 export * from '@testing-library/react'
 export { default as userEvent } from '@testing-library/user-event'
 export { customRender as render }
+export { getByTextContent }
