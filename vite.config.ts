bec8b17bb6b832ef697fd290edf847e72d5e7e9e
jéssica senger <jessicasbogoni1@gmail.com>
2023-01-10 23:03:54 -0400

    test config with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import vitePluginFaviconsInject from 'vite-plugin-favicons-inject'
@@ -10,4 +10,12 @@ export default defineConfig({
     svgr(),
     vitePluginFaviconsInject('./src/assets/favicon.png'),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+       reporter: ['text', 'json', 'html'],
+    }
+  }
 })
